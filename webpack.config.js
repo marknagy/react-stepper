9af9d3a9f233bab5fa8a28507406940373669a32
Mark Nagy <mark.nagy@prezi.com>
2016-10-01 02:30:33 +0200

    clean up .gitignore + add missing files

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	entry: './main.js',
+	output: {
+		path: './',
+		filename: 'bundled.js'
+	},
+	devServer: {
+		inline: true,
+		port: 3334,
+
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	}
+}
